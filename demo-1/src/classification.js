ae763479ecca8e7900de47cd320777338e8e46f5
glaucia86 <glaucia_lemos86@hotmail.com>
2019-10-08 22:19:20 -0300

    Criando o arquivo 'classification.js'

---
+++ demo-1/src/classification.js
@@ -0,0 +1,7 @@
+/**
+ * arquivo: src/facedetection.js
+ * Data: 18/11/2019
+ * Descrição: arquivo responsável por realizar a classificação de uma imagem com Custom Vision 
+ *  & Node.js
+ * Author: Glaucia Lemos
+ */
